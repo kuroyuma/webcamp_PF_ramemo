e0cd09ef2b786f15239e04423a2dd2ea6ccea75a
kuroyuma <k.1993.yuma.0324@gmail.com>
2021-11-20 08:09:16 +0000

    [fix]コメント非同期

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,7 +2,6 @@ class PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-
     @comment = current_user.post_comments.new(post_comment_params)
     #上記はこの2行と同じcomment = PostComment.new(post_comment_params)
     # 　　　　　　　　　comment.user_id = current_user.id
